4849456d821c38f0f4ede82a1ddf2458d268aef2
Rece Cristian <rece.cristian@gmail.com>
2023-10-28 12:12:02 +0300

    working on models of the database: 2

--- back-end/models/Index.js
+++ back-end/models/Index.js
@@ -1,13 +1,13 @@
 const db = require("../config/db")
-const Sequelize = require("sequelize")
+const {Sequelize} = require("sequelize")
 
-const Park = require("./park")
-const Review = require("./review")
-const User = require("./user")
+const ParkModel = require("./Park")
+const ReviewModel = require("./Review")
+const UserModel = require("./User")
 
-const user = UserModel(db, Sequelize)
-const park = ParcModel(db, Sequelize)
-const review = ReviewModel(db, Sequelize)
+const user = new UserModel(db, Sequelize)
+const park = new ParkModel(db, Sequelize)
+const review = new ReviewModel(db, Sequelize)
 
 module.exports = {
     park,
--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,25 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-const db = require('./models')
+const db = require("./models/Index")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/reset',(req,res) => {
-  res.send("Salutare!");
-  
+app.get("/reset", (req, res) => {
+    res.send("Salutare!")
+    try {
+        db.sync({ force: true })
+        res.status(200)
+        res.send("Database has been reset!")
+    } catch {
+        res.status(500)
+        res.send("Server Error! Couldn't reset database...")
+    }
 })
